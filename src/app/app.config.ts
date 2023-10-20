af75161e870a4dc31413acb6afde36353126817f
an_gular <v.ivahnenko@outlook.com>
2023-10-20 20:55:05 +0300

    added loacl-storage-service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { TuiRootModule } from '@taiga-ui/core';
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
